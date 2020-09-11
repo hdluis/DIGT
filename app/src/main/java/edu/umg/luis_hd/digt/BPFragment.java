0b7e1f762cfb82aacb265121ee230ff633f485e0
Luis Velásquez <luishd.10@gmail.com>
2020-09-10 19:12:56 -0600

    Final

---
+++ app/src/main/java/edu/umg/luis_hd/digt/BPFragment.java
@@ -0,0 +1,25 @@
+package edu.umg.luis_hd.digt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class BPFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_delitosr, container, false);
+        View v = inflater.inflate(R.layout.fragment_bp, container, false);
+        WebView webView = (WebView)v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://lumivr11.000webhostapp.com/buenasprac2.html");
+        return v;
+    }
+}
---
+++ app/src/main/java/edu/umg/luis_hd/digt/defaultFragment.java
@@ -0,0 +1,25 @@
+package edu.umg.luis_hd.digt;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class defaultFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_ad, container, false);
+        View v = inflater.inflate(R.layout.fragment_ad, container, false);
+        WebView webView = (WebView)v.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/def.php");
+        return v;
+    }
+}
